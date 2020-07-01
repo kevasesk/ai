deffbb3fb2a2f880395a56acb0009d6d0a373875
eugenesm <eugenesm@ronis.mail>
2020-07-01 20:38:46 +0300

    add draw field

--- gui.py
+++ gui.py
@@ -1,7 +1,11 @@
 import sys  # sys нужен для передачи argv в QApplication
 import os
 import numbersGUI # import my gui file
-from PyQt5 import QtWidgets
+
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView
+from PyQt5.QtGui import QBrush, QPen, QPainter, QPolygon, QPixmap, QImage, QColor
+from PyQt5.QtCore import QPoint, Qt
 
 class NumbersApp(QtWidgets.QMainWindow, numbersGUI.Ui_MainWindow):
     def __init__(self):
@@ -13,6 +17,37 @@ class NumbersApp(QtWidgets.QMainWindow, numbersGUI.Ui_MainWindow):
         self.pushButton_2.clicked.connect(self.learn)
         self.pushButton_3.clicked.connect(self.activate)
 
+        self.horizontalSlider.setValue(0)
+        self.horizontalSlider_2.setValue(0)
+        self.horizontalSlider_3.setValue(0)
+        self.horizontalSlider_4.setValue(0)
+        self.horizontalSlider_5.setValue(0)
+        self.horizontalSlider_6.setValue(0)
+        self.horizontalSlider_7.setValue(0)
+        self.horizontalSlider_8.setValue(0)
+        self.horizontalSlider_9.setValue(0)
+        self.horizontalSlider_10.setValue(0)
+
+        self.scene = QGraphicsScene()
+        view = QGraphicsView(self.scene, self)
+        view.setGeometry(0,0,400,400)
+        self.drawRect(0,0,400,400)
+
+    def drawRect(self, x1, y1, x2, y2):
+        redBrush = QBrush(Qt.red)
+        blackPen = QPen(Qt.black)
+        #ellipse = self.scene.addRect(x1, y1, x2, y2, blackPen, redBrush)
+
+        width, height = 400, 400
+        im = QImage(width, height, QImage.Format_ARGB32)
+
+        for x in range(im.width()):
+            for y in range(im.height()):
+                im.setPixel(x, y, QColor(255, x/4 * 2.56, y/4 * 2.56, 255).rgb())
+
+        self.scene.addPixmap(QPixmap.fromImage(im))
+
+
     def clear(self):
         self.textEdit.setText('clear')
 
@@ -21,7 +56,6 @@ class NumbersApp(QtWidgets.QMainWindow, numbersGUI.Ui_MainWindow):
 
     def activate(self):
         self.textEdit.setText('Activate')
-
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
